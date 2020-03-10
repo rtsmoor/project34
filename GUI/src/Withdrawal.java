8d440b6b35efb57ffcb0cf89a846356505751ef0
rtsmoor <57296077+rtsmoor@users.noreply.github.com>
2020-03-10 15:58:09 +0100

    /-Edited Classes-\
    -Balance.java
     -added variables and methods
    -Withdrawal.java
     -added variables and methods

--- GUI/src/Withdrawal.java
+++ GUI/src/Withdrawal.java
@@ -1,3 +1,18 @@
+import java.math.BigDecimal;
+
 public class Withdrawal extends Balance {
+    private int amount = 0;
+
+    int fastWithdrawal(){
+    amount = 70;
+    return amount;
+}
+
+    int customWithdrawal(int input){
+        double balance = amount = input;
+        withdrawBalance(amount);
+
+        return amount;
+    }
 
 }
