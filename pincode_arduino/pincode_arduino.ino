990481d9c581b879bf2e17c21a3af5078688e2be
Freek Visser <freek@sacrome.com>
2020-03-18 22:21:11 +0100

    add functionality of linking pass to pincode

--- pincode_arduino/pincode_arduino.ino
+++ pincode_arduino/pincode_arduino.ino
@@ -1,6 +1,13 @@
 #include <Keypad.h>
 #include <Wire.h>
 #include <MD5.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -11,16 +18,17 @@ char hexaKeys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-bool pinCheck = false;
+bool cardPresented = false;
+bool finished = false;
 int pinCount = 0;
 int i = 0;
 int checkDatabase = 0;
 long enteredCode = 0;
 char *md5str;
 char enteredCodeArray[4] = {'0', '0', '0', '0'};
-char pinCode[4] = {'2','4','5','1'};
-byte rowPins[ROWS] = {9,8,7,6}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {5,4,3,2}; //connect to the column pinouts of the keypad
+String passUID= "";
+byte rowPins[ROWS] = {8,7,6,5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {4,3,2,1}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
@@ -32,29 +40,71 @@ void setup(){
   Serial.begin(9600);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-  Serial.print("Please enter pincode: ");
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+  
+  Serial.print("Please present your card: ");
 }
   
 void loop(){
+
+    // Look for new cards
+   if(cardPresented == false){
+    if ( ! mfrc522.PICC_IsNewCardPresent()) 
+    {
+      return;
+    }
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) 
+    {
+      return;
+    }
+    //Show UID on serial monitor
+    String content= "";
+    byte letter;
+    for (byte i = 0; i < mfrc522.uid.size; i++) 
+    {
+       /*Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+       Serial.print(mfrc522.uid.uidByte[i], HEX);*/
+       content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+       content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+  
+    content.toUpperCase();
+    passUID = content.substring(1);
+    Serial.println(passUID);
+    cardPresented = true;
+
+    Serial.print("Please enter pincode: ");
+  } 
+  
+  //Button is pressed
   char customKey = customKeypad.getKey();
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
   if (customKey){
-    
+
+
+    //Register code
     if(customKey == '#'){
       Serial.println();
       for(int x = 0; x < i; x++){
         int currentNumber = enteredCodeArray[(i-x)-1] - '0';
         enteredCode += currentNumber * round(pow(10, x));
       }
+
+
+      //Hashing
+      //Serial.print("Hashing: "); Serial.print(enteredCode);
       unsigned char* hash=MD5::make_hash(enteredCode);
       //generate the digest (hex encoding) of our hash
       md5str = MD5::make_digest(hash, 16);
+      //Serial.println(md5str);
       free(hash);
-      checkDatabase = 1;
+
+      //Start check
       Serial.println("Authorizing...");
       sendEvent();
-      
+      finished = true;
       delay(3000);
     }
 
@@ -62,7 +112,7 @@ void loop(){
     if(customKey != '*' || customKey != 'D'){
     enteredCodeArray[i] = customKey;
     if(i >= 0){
-      Serial.print("*");
+      Serial.print('*');
     }
     }
     if(customKey == 'D'){   //char D is deleting the whole line
@@ -81,26 +131,42 @@ void loop(){
   }
 }
 
+
+//Send status
 void sendEvent(){
-  Wire.write(checkDatabase);
+  byte response [11];
+  if(passUID != 0 || passUID != ""){
+    for(byte i = 0; i < 11; i++){
+      response[i] = (byte)passUID.charAt(i);
+    }
+    Wire.write(response, sizeof(response)); 
+  }
+  passUID = "";
 }
 
+
+//Receive hashed pincode from database via ESP8266
 void receiveEvent(int howMany){
   String receivedCode = "";
   while(Wire.available()){
     char b = Wire.read();
     receivedCode += b;
   }
-  passwordControl(receivedCode);
+  if(enteredCode != 0 && finished){
+    passwordControl(receivedCode);
+  }
 }
 
+
+//Compare hashed database pincode and hashed entered code
 void passwordControl(String pincode){
-  if(enteredCode != 0){
     String checkCode(md5str);
     if(checkCode == pincode){
+      //Serial.print(checkCode); Serial.print(" : "); Serial.println(pincode);
       Serial.println("Succes");
     }
     else{
+      //Serial.print(checkCode); Serial.print(" : "); Serial.println(pincode);
       Serial.println("Access denied");
     }
     if(i != 0){
@@ -108,8 +174,8 @@ void passwordControl(String pincode){
       }
     checkDatabase = 0;
     enteredCode = 0;
+    cardPresented = false;
 
     Serial.println();
     Serial.print("Please enter pincode: ");
-  }
 }
